286f8f250863c35bf90ad1ed2b7b49476cacbff2
Vaibhav Puri <vaibhav.puri26@gmail.com>
2024-11-27 22:59:30 +0530

    Updated Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -55,7 +55,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2023 Inovotek Coding Academy, Inc. All rights reserved.
+            &copy; 2024 Vaibhav Puri, Inc. All rights reserved.
           </p>
         </div>
       </div>
